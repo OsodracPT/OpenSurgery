83bc8b9ade2c7400b9be95feba59f9d12254ab23
Pedro Cardoso <pedroosodracwork@gmail.com>
2017-10-19 18:19:36 +0100

    Login fully functional

---
+++ Main Project/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Main_Project
+{
+    class Login
+    {
+        public static int TryLogin(string username, string password)
+        {
+            int i = 0;
+
+
+            DataSet dsUser = DBConnection.getDBConnectionInstance().getDataSet(Constants.selectLogin(username, password));
+
+            //get the table to be displayed from the data set
+            DataTable dtUser = dsUser.Tables[0];
+
+            i = Convert.ToInt32(dtUser.Rows.Count.ToString());
+            return i;
+        }
+
+    }
+}
+
